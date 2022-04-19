8d23a64b437d4f68e52370d5d620654a458d99ff
Horlakz <horlakz@protonmail.com>
2022-04-19 23:00:32 +0100

    init

---
+++ app/routes/index.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Remix!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed totam porro quaerat omnis unde esse sapiente, voluptates tenetur cumque corporis facere quod quas, voluptatum facilis, odit commodi? Totam, error unde.
+      </p>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/routes/posts/new.jsx
@@ -0,0 +1,89 @@
+import { Link, redirect, useActionData, json } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+
+function validateTitle(title) {
+  if (typeof title !== "string" || title.length < 3) {
+    return "Title must be at least 3 characters";
+  }
+}
+
+function validateBody(body) {
+  if (typeof body !== "string" || body.length < 20) {
+    return "Body must be at least 20 characters";
+  }
+}
+
+function badRequest(data) {
+  return json(data, { status: 400 })
+}
+
+
+export const action = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+  const user = await getUser(request);
+
+  const fields = { title, body };
+
+  const fieldErrors = {
+    title: validateTitle(title),
+    body: validateBody(body),
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    console.log(fieldErrors);
+    return badRequest({ fieldErrors, fields })
+  }
+
+  const post = await db.post.create({ data: {...fields, userId: user.id} })
+
+  return redirect(`/posts/${post.id}`)
+
+};
+
+function NewPost() {
+  const actionData = useActionData();
+
+  return (
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" defaultValue={actionData?.fields?.title} />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.title &&
+                  actionData?.fieldErrors?.title}
+              </p>
+            </div>
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea type="text" name="body" id="body" defaultValue={actionData?.fieldErrors?.body} />
+            <div className="error">
+              <p>
+                {actionData?.fieldErrors?.body &&
+                  actionData?.fieldErrors?.body}
+              </p>
+            </div>
+          </div>
+          <button type="submit" className="btn btn-block">
+            Add Post
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default NewPost;
